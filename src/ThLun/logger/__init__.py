fe5e90ab488d3ffb1eb8f4b732bac43034a39941
Netronis <gamerolikam@gmail.com>
2025-11-10 17:00:44 +0200

    Global edits in logger

--- src/ThLun/logger/__init__.py
+++ src/ThLun/logger/__init__.py
@@ -3,10 +3,11 @@ Initialization module for ThLun's logger module.
 Contains the Logger class and the LogLevel dataclass.
 """
 
-from .logger import Logger
+from .logger import Logger, Formatter
 from .types import LogLevel
 
 __all__ = [
     "Logger",
+    "Formatter",
     "LogLevel",
 ]
